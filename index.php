568f6432817471a16229b1dd9eee4b4e51f3e80a
ruslan123 <perishemizade@gmail.com>
2023-03-27 11:20:00 +0400

    contants hazir

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// 1. define(olan sozu cumlede qaytarir)
+// define("GREETING","Hello you! How are you today?");
+// echo constant("GREETING");
+// 2. arraylerde
+// define("languages",[
+//    "html", 
+//    "css",
+//    "js",
+// ]);
+// echo languages[1]
+//3.define("ruslan","Hello bro"); buradaki ruslani function-nun icerisinde tanimlaya bilirik
+?>
